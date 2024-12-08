6d0e5f4e98c7f69ab47f78a3a62ba883fd10699e
adel4780 <adelk138019@gmail.com>
2024-12-08 11:53:32 +0330

    Add mesh network functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,51 @@
 #include <Arduino.h>
+#include <painlessMesh.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Defines
+#define MESH_PREFIX     "ParkingMesh"
+#define MESH_PASSWORD   "password123"
+#define MESH_PORT       5555
 
+// Variables
+painlessMesh mesh;
+
+// Function declarations:
+void sendMessage();
+void receivedCallback(uint32_t from, String &msg);
+void newConnectionCallback(uint32_t nodeId);
+void changedConnectionCallback();
+void nodeTimeAdjustedCallback(int32_t offset);
+
+// Setup function
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
-}
+  Serial.begin(115200);
+}  
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+// Function definitions:
+// Function to broadcast free parking spots to the mesh
+void sendMessage() {
+  Serial.println("Message broadcasted");
+}
+// Callback for receiving messages from the mesh
+void receivedCallback(uint32_t from, String &msg) {
+  Serial.printf("Received from %u: msg=%s\n", from, msg.c_str());
+}
+
+// Callback for new connections
+void newConnectionCallback(uint32_t nodeId) {
+  Serial.printf("New connection established, nodeId = %u\n", nodeId);
+}
+
+// Callback for changed connections
+void changedConnectionCallback() {
+  Serial.println("Connections updated");
+}
+
+// Callback for adjusted node time
+void nodeTimeAdjustedCallback(int32_t offset) {
+  Serial.printf("Node time adjusted. Offset = %d\n", offset);
 }
