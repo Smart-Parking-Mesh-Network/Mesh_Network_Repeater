fd6e1db6ff5db1998f3007196c085335d5bfcf9b
adel4780 <adelk138019@gmail.com>
2024-12-08 12:02:25 +0330

    Add entrance score

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,15 @@
 
 // Variables
 painlessMesh mesh;
+Scheduler userScheduler;
+
+// Struct to store received data
+struct SectionData {
+  int freeSpots = -1;
+  int entranceScore = 0;
+};
+
+std::map<String, SectionData> receivedSpots;
 
 // Function declarations:
 void sendMessage();
@@ -16,23 +25,73 @@ void newConnectionCallback(uint32_t nodeId);
 void changedConnectionCallback();
 void nodeTimeAdjustedCallback(int32_t offset);
 
+// Task for sending messages
+Task taskSendMessage(TASK_SECOND * 1, TASK_FOREVER, &sendMessage);
+
 // Setup function
 void setup() {
   Serial.begin(115200);
+
+  // Mesh initialization
+  mesh.setDebugMsgTypes(ERROR | STARTUP); // Debug messages
+  mesh.init(MESH_PREFIX, MESH_PASSWORD, &userScheduler, MESH_PORT);
+  mesh.onReceive(&receivedCallback);
+  mesh.onNewConnection(&newConnectionCallback);
+  mesh.onChangedConnections(&changedConnectionCallback);
+  mesh.onNodeTimeAdjusted(&nodeTimeAdjustedCallback);
+
+  // Add and enable tasks
+  userScheduler.addTask(taskSendMessage);
+  taskSendMessage.enable();
 }  
 
 void loop() {
-
+  mesh.update();
 }
 
 // Function definitions:
 // Function to broadcast free parking spots to the mesh
 void sendMessage() {
-  Serial.println("Message broadcasted");
+  String msg;
+
+  // Append data for each section: "A 5 B 3 C 4 D 6 E 2"
+  for (const auto &section : receivedSpots) {
+    if (section.second.freeSpots != -1) {
+      msg += section.first + " " + String(section.second.freeSpots) + " " +
+             String(section.second.entranceScore) + " ";
+    }
+  }
+  mesh.sendBroadcast(msg);
+  // taskSendMessage.setInterval(random(TASK_SECOND * 1, TASK_SECOND * 5)); // Randomize broadcast interval
+  Serial.println("Sending: " + msg);
 }
+
 // Callback for receiving messages from the mesh
 void receivedCallback(uint32_t from, String &msg) {
-  Serial.printf("Received from %u: msg=%s\n", from, msg.c_str());
+  Serial.printf("Received from %u msg=%s\n", from, msg.c_str());
+
+  unsigned int index = 0;
+  while (index < msg.length()) {
+    int spaceIdx = msg.indexOf(' ', index);
+    if (spaceIdx == -1) break;
+
+    String section = msg.substring(index, spaceIdx);
+    index = spaceIdx + 1;
+
+    spaceIdx = msg.indexOf(' ', index);
+    int spots = msg.substring(index, spaceIdx).toInt();
+    index = spaceIdx + 1;
+    
+    // read entrance Score
+    // Note: The End condition is for last score -> (spaceIdx == -1)
+    spaceIdx = msg.indexOf(' ', index); 
+    int entranceScore = (spaceIdx == -1) ? msg.substring(index).toInt() : msg.substring(index, spaceIdx).toInt();
+    index = (spaceIdx == -1) ? msg.length() : spaceIdx + 1;
+     
+    receivedSpots[section] = {spots, entranceScore};
+    Serial.printf("Updated section %s: spots=%d, entrance=%d\n", section.c_str(), spots, entranceScore);
+
+  }
 }
 
 // Callback for new connections
